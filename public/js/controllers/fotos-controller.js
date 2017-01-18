eeaa86216c5bf733d14cddf517c1866ff429996d
Diego Vieira <diegoavieira@gmail.com>
2017-01-17 23:02:13 -0200

    cap 2

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+	$scope.foto = {
+		titulo : 'Leão',
+		url : 'http://www.astrocentro.com.br/files_astrocentrobr/field/image/astrologia-astrocentro_1416.jpg'
+	};
+});
